2aac60309acd45d76ed2560b00b22824ebc95538
Rojay Ingram <rojayingramlearns@gmail.com>
2025-03-14 17:25:54 -0500

    initial commit

---
+++ components/ScreenWrapper.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { View, Text } from 'react-native-web';
+
+const ScreenWrapper = ({children, bg}) => {
+
+    const {top} = useSafeAreaInsets();
+    const paddingTop = top>0? top+5: 30;
+
+    return (
+        <View style={{flex:1, paddingTop, background: bg}}>
+            {
+                children
+            }
+        </View>
+    );
+}
+
+export default ScreenWrapper;
